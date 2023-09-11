2af9766a1b52f13a2027ac58791335b28d489d04
thongkhoav <khoaxilo951@gmail.com>
2023-09-11 22:34:24 +0700

    init front end structure without redux

---
+++ frontend/src/pages/user/Home/Home.tsx
@@ -0,0 +1,8 @@
+import MainLayout from "src/layouts/MainLayout";
+export default function Home() {
+  return (
+    <MainLayout>
+      <h2 className="text-green-600">Home</h2>
+    </MainLayout>
+  );
+}
