951699937dbdc6b7f77c73c473bbd7181787af49
thongkhoav <khoaxilo951@gmail.com>
2023-10-19 15:44:45 +0700

    check mentor role

--- frontend/src/pages/mentor/modal-blog/ModalBlog.tsx
+++ frontend/src/pages/mentor/modal-blog/ModalBlog.tsx
@@ -22,7 +22,7 @@ export default function ModalBlog({ blogDetail, onClose, handleUpdateBlog, ...pr
           {blogDetail?.status === "waiting" ? "Chờ duyệt" : "Đã từ chối"}
         </p>
       </Col>
-      <Col span={8} style={{ textAlign: "right"}}>
+      <Col span={8} style={{ textAlign: "right" }}>
         {blogDetail?.status === "waiting" && (
           <Button
             className="btn-reject"
@@ -36,7 +36,7 @@ export default function ModalBlog({ blogDetail, onClose, handleUpdateBlog, ...pr
         )}
         <Button
           className="btn-primary"
-          style={{ marginLeft: "6px",marginRight:'30px' }}
+          style={{ marginLeft: "6px", marginRight: "30px" }}
           icon={<CheckOutlined />}
           onClick={() => {
             handleAccept();
@@ -51,7 +51,10 @@ export default function ModalBlog({ blogDetail, onClose, handleUpdateBlog, ...pr
   const handleReject = async () => {
     let dataSend = { status: "rejected" };
     try {
-      const res = await axiosPrivate.post(`/api/v1/blogs/mentor/waiting-blogs/${blogDetail._id}`,dataSend);
+      const res = await axiosPrivate.post(
+        `/api/v1/blogs/mentor/waiting-blogs/${blogDetail._id}`,
+        dataSend
+      );
       if (res.data.status === "success") {
         handleUpdateBlog("rejected", blogDetail._id);
         toast.success("Từ chối thành công!", toastOption);
@@ -65,7 +68,10 @@ export default function ModalBlog({ blogDetail, onClose, handleUpdateBlog, ...pr
   const handleAccept = async () => {
     let dataSend = { status: "public" };
     try {
-      const res = await axiosPrivate.post(`/api/v1/blogs/mentor/waiting-blogs/${blogDetail._id}`,dataSend);
+      const res = await axiosPrivate.post(
+        `/api/v1/blogs/mentor/waiting-blogs/${blogDetail._id}`,
+        dataSend
+      );
       if (res.data.status === "success") {
         handleUpdateBlog("public", blogDetail._id);
         toast.success("Duyệt thành công!", toastOption);
@@ -77,44 +83,44 @@ export default function ModalBlog({ blogDetail, onClose, handleUpdateBlog, ...pr
   };
 
   return (
-    <>
-      <Modal
-        className="modal_container"
-        title={modalTitle}
-        centered
-        width={"70%"}
-        onCancel={onClose}
-        footer={false}
-        {...props}
-      >
-        {blogDetail && (
-          <>
-            {blogDetail.thumbnail && (
-              <img src={blogDetail.thumbnail} alt="" className="h-[500px] w-full mb-2" />
-            )}
-            <h1 className="text-2xl">Title: {blogDetail.title}</h1>
-            <div className="flex justify-between items-center">
-              <h2 className="capitalize text-lg font-medium text-[#404040] opacity-80">
-                <span>Thể loại: {blogDetail.blogCateId?.name}</span>
-              </h2>
-            </div>
-            <div className="mb-2">
-              Tag:
-              {blogDetail?.blogTagIds?.map((tag: any) => (
-                <Link
-                  to={`${PATH.BLOG}?tag=${tag.name}`}
-                  key={tag._id}
-                  className="text-sm text-inherit px-3 py-2 mr-3 rounded-sm underline hover:opacity-100 text-gray-800 opacity-80"
-                >
-                  {tag.name}
-                </Link>
-              ))}
-            </div>
-            <hr />
-            <div className="mt-2">{blogDetail.contentRaw && parse(`${blogDetail.contentRaw}`)}</div>
-          </>
-        )}
-      </Modal>
-    </>
+    <Modal
+      className="modal_container"
+      title={modalTitle}
+      centered
+      width={"70%"}
+      onCancel={onClose}
+      footer={false}
+      {...props}
+    >
+      {blogDetail && (
+        <>
+          {blogDetail.thumbnail ? (
+            <img src={blogDetail.thumbnail} alt="" className="h-[500px] w-full mb-2" />
+          ) : (
+            <p className="my-3 text-center"> Chưa có ảnh đại điện blog</p>
+          )}
+          <h1 className="text-2xl">Title: {blogDetail.title}</h1>
+          <div className="flex justify-between items-center">
+            <h2 className="capitalize text-lg font-medium text-[#404040] opacity-80">
+              <span>Thể loại: {blogDetail.blogCateId?.name}</span>
+            </h2>
+          </div>
+          <div className="mb-2">
+            Tag:
+            {blogDetail?.blogTagIds?.map((tag: any) => (
+              <Link
+                to={`${PATH.BLOG}?tag=${tag.name}`}
+                key={tag._id}
+                className="text-sm text-inherit px-3 py-2 mr-3 rounded-sm underline hover:opacity-100 text-gray-800 opacity-80"
+              >
+                {tag.name}
+              </Link>
+            ))}
+          </div>
+          <hr />
+          <div className="mt-2">{blogDetail.contentRaw && parse(`${blogDetail.contentRaw}`)}</div>
+        </>
+      )}
+    </Modal>
   );
 }
