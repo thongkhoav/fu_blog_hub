a9da70adab3988089207d8fee054727b27e5a62f
LinhHoang <hoanglinh9955@gmail.com>
2023-10-24 21:22:19 +0700

    comment

--- frontend/src/components/Comment/DropDown/DropDown.tsx
+++ frontend/src/components/Comment/DropDown/DropDown.tsx
@@ -1,15 +1,9 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { EllipsisOutlined } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import { Dropdown, message, Space } from "antd";
-
-const onClick: MenuProps["onClick"] = ({ key }) => {
-  if (key == "1") {
-    message.info(`Edit`);
-  } else {
-    message.info(`Remove`);
-  }
-};
+import useAxiosPrivate from "~/config/useAxiosPrivate";
+import { useAuth } from "~/utils/helpers";
 
 const items: MenuProps["items"] = [
   {
@@ -22,14 +16,86 @@ const items: MenuProps["items"] = [
   }
 ];
 
-const DropDown = () => (
-  <Dropdown menu={{ items, onClick }}>
-    <a onClick={e => e.preventDefault()}>
-      <Space>
-        <EllipsisOutlined />
-      </Space>
-    </a>
-  </Dropdown>
-);
+function DropDown({comment, parentData, updateRenderList, setEdit, Edit}: {comment: any, parentData: any, updateRenderList: any, setEdit: any, Edit: any}){
+  const axiosPrivate = useAxiosPrivate();
+  const { userGlobal } = useAuth();
+
+  const onClick: MenuProps["onClick"] = async ({ key }) => {
+
+    if(!userGlobal){
+      return;
+    }
+    const res = await axiosPrivate.put(`/api/v1/comment/check/${comment.id}`, {
+      userId: userGlobal._id as string,
+    });
+    const data = res.data.data
+    console.log(res)
+    
+    if(key == "1"){
+      if(!data){
+        return;
+      }
+      const list = parentData.map((com: any) => {
+        if (com.id == comment.id) {
+          //check parent comment
+          console.log(com)
+          com.editStatus = !com.editStatus;
+          setEdit(!Edit);
+          return com; 
+        } else if(com.children.length > 0) {
+         //if parent dont have check child
+            let childComment = com.children.map((child: any) =>{
+              if(child.id == comment.id){
+                child.editStatus = !child.editStatus;
+                setEdit(!Edit);
+                return child
+              }else{
+                return child;
+              }
+            })
+            return childComment;
+          //paren
+        }else{
+          return com;
+        }
+      });
+      updateRenderList(list);
+      
+    }else{
+      if(!data){
+        return;
+      }
+      //change status comment
+      const confirmed = window.confirm(`Bạn Có Muốn Xóa Bình Luận Này?`);
+      if (confirmed) {
+        //call api to change status comment
+        const res = await axiosPrivate.put(`/api/v1/comment/delete/${comment.id}`);
+        const data = res.data.data
+   
+        const list = parentData.map((com: any) => {
+          if (com.id == comment.id) {
+            return data;
+          } else {
+            return com;
+            
+          }
+        });
+        updateRenderList(list);
+        window.location.reload();
+      }
+
+    }  
+};
+  
+  return (
+    <Dropdown menu={{ items, onClick }}>
+      <a onClick={e => e.preventDefault()}>
+        <Space>
+          <EllipsisOutlined />
+        </Space>
+      </a>
+    </Dropdown>
+  );
+}
 
 export default DropDown;
